1f297452d1146dc7d0231f53a1b9cd8299b9a1b1
= <=>
2023-09-12 19:27:37 -0700

    primer commit, agregado back de categorias

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Categoria::all());
+    }
+
+    public function store(Request $request)
+    {
+        $categoria = Categoria::create($request->post());
+        return response()->json($categoria);
+    }
+
+    public function show(int $categoria)
+    {
+        return response()->json(Categoria::find($categoria));
+    }
+
+    public function update(Request $request, Categoria $categoria)
+    {
+        $categoria->fill($request->post());
+        return response()->json($categoria);
+    }
+
+    public function destroy(Categoria $categoria)
+    {
+        $categoria->delete();
+        return response()->json("done");
+    }
+}
